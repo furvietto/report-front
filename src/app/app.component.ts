77eaf53a21a42dafc1e8a830aea8909e5cd74a87
Fulvio Gaudenzi <f.gaudenzi@reply.it>
2023-01-16 11:05:44 +0100

    add oauth config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { OAuthModule,AuthConfig } from 'angular-oauth2-oidc';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'report-front';
+
+  constructor(private oAuthModule: OAuthModule) {}
+
+    authConfig: AuthConfig = {
+      // Url of the Identity Provider
+      issuer: 'http://localhost:8180/realms/idReports',
+
+      // URL of the SPA to redirect the user to after login
+      redirectUri: window.location.origin,
+
+      // The SPA's id. The SPA is registerd with this id at the auth-server
+      // clientId: 'server.code',
+      clientId: 'idreports-frontend',
+
+      // Just needed if your auth server demands a secret. In general, this
+      // is a sign that the auth server is not configured with SPAs in mind
+      // and it might not enforce further best practices vital for security
+      // such applications.
+      // dummyClientSecret: 'secret',
+
+      responseType: 'code',
+
+      // set the scope for the permissions the client should request
+      // The first four are defined by OIDC.
+      // Important: Request offline_access to get a refresh token
+      // The api scope is a usecase specific one
+      scope: 'openid profile email offline_access',
+
+      showDebugInformation: true,
+    };
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from "@angular/forms";
+
+import {OAuthModule} from 'angular-oauth2-oidc'
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    OAuthModule.forRoot({
+      resourceServer: {
+          allowedUrls: ['http://localhost:8080/report/v1/client'],
+          sendAccessToken: true
+      }
+  })
   ],
   providers: [],
   bootstrap: [AppComponent]
